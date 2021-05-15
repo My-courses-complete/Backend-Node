aaf3e791432da3389b3f75e344f88393dbaf6533
JMR-Mejia <chucho4720013@gmail.com>
2021-05-15 11:17:41 -0500

    Implementando Boom

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,16 +2,27 @@ const express = require('express');
 
 const { config } = require('./config');
 const moviesApi = require('./routes/movies');
-const { errorHandler, logErrors } = require('./utils/middleware/errorHandler');
+const {
+  errorHandler,
+  logErrors,
+  wrapError,
+} = require('./utils/middleware/errorHandler');
+const notFoundHandler = require('./utils/middleware/notFounbHandler');
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 
+// routes
 moviesApi(app);
 
+// Catch 404
+app.use(notFoundHandler)
+
+// Errores Middlewares
 app.use(errorHandler);
+app.use(wrapError);
 app.use(logErrors);
 
 app.listen(config.port, () => {
